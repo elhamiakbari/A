94813243d506df39d70ce5fd41d1e430c6950f2c
elhamiakbari <93420421+elhamiakbari@users.noreply.github.com>
2021-11-15 13:17:28 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,97 @@
-let now = new Date();
+//day format
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
+function formatDay(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
 
-let day = days[now.getDay()];
+  //month format
 
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
 
-let month = months[now.getMonth()];
-
-let date = now.getDate();
-
-let year = now.getFullYear();
-
-let heading = document.querySelector("h1");
-heading.innerHTML = `Today is ${day}, ${month} ${date}, ${year}`;
-
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
+  let currentDay = days[date.getDay()];
+  let currentDate = date.getDate();
+  let currentMonth = months[date.getMonth()];
+  let currentHours = date.getHours();
+  let currentMinutes = date.getMinutes();
+  if (currentMinutes < 10) {
+    currentMinutes = "0".concat(currentMinutes);
+  }
+  return `${currentDay}, ${currentDate}. ${currentMonth} ${currentHours}:${currentMinutes}`;
 }
 
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
+let dateElement = document.querySelector("#current-time");
+let currentTime = new Date();
+dateElement.innerHTML = formatDay(currentTime);
+
+//search for city name, display weather
+function displayWeatherCondition(response) {
+  console.log(response.data);
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
 }
 
-let currentTime = document.querySelector(".time");
-currentTime.innerHTML = `${hours}:${minutes}`;
+function searchCity(city) {
+  let apiKey = "008d27c5822351d9ca356a6720593b1c";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
-function search(event) {
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function inputCity(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-text-input");
-  let city = document.querySelector(".city");
-  city.innerHTML = `${searchInput.value}`;
+
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
 }
 
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", search);
 
-function changeUnit(event) {
+
+function searchLocation(position) {
+  let apiKey = "fef90cf423cdd1a82c0f177876085211";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
   event.preventDefault();
-  let temp = document.querySelector(".current-temp");
-  temp.innerHTML = "66°F";
-  unit.innerHTML = "°C";
+  navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
-let unit = document.querySelector(".unit");
+let submit = document.querySelector("#city-form");
+submit.addEventListener("submit", inputCity);
 
-unit.addEventListener("click", changeUnit);
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("New York");
